1314462199684403633e8cd179e26576c6f19307
Swadesh Chatterjee <swadeshchatterjee512@gmail.com>
2026-01-28 18:43:48 +0530

    add core entities for room management

---
+++ src/main/java/com/bookingsystem/entity/Inventory.java
@@ -0,0 +1,47 @@
+package com.bookingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "inventories")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "room_id", nullable = false)
+    private Room room;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false, columnDefinition = "INTEGER DEFAULT 0")
+    private Integer bookedCount;
+    @Column(nullable = false)
+    private Integer totalCount;
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal surgeFactor;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private Boolean closed;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/bookingsystem/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.bookingsystem.entity.enums;
+
+public enum Role {
+    GUEST,
+    HOTEL_MANAGER
+}
