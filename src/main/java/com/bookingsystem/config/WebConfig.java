e725e52b87cced2263ce37e95000dbaa65c27d72
Swadesh Chatterjee <swadeshchatterjee512@gmail.com>
2026-02-15 00:12:07 +0530

    complete the webconfig

--- src/main/java/com/bookingsystem/config/WebConfig.java
+++ src/main/java/com/bookingsystem/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.bookingsystem.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
